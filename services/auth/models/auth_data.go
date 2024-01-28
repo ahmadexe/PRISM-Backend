7dc725b1adb37508876f975aab9130e75fec2832
ahmadexe <muahmad710@gmail.com>
2024-01-29 03:49:12 +0500

    chore: timestamp fixes

--- services/auth/models/auth_data.go
+++ services/auth/models/auth_data.go
@@ -1,6 +1,10 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type AuthData struct {
 	Id        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
@@ -8,4 +12,15 @@ type AuthData struct {
 	Fullname  string             `json:"fullname" bson:"fullname"`
 	Domain    string             `json:"domain" bson:"domain"`
 	CreatedAt int64              `json:"createdAt" bson:"createdAt"`
+}
+
+func (authData *AuthData) UnmarshalJSON([]byte) error  {
+	// check if createdAt is not null
+	// if null, set createdAt to current time
+	// else, set createdAt to the value of createdAt
+
+	if authData.CreatedAt == 0 {
+		authData.CreatedAt = time.Now().UnixMicro()
+	}
+	return nil
 }
