92f381959a31a3f5a829b3f54152c59f5653f079
ahmadexe <muahmad710@gmail.com>
2024-01-29 00:57:34 +0500

    chore: auth model formatting

--- services/auth/models/auth_data.go
+++ services/auth/models/auth_data.go
@@ -3,9 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AuthData struct {
-	Id primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Email string `json:"email" bson:"email"`
-	Fullname string `json:"fullname" bson:"fullname"`
-	Domain string `json:"domain" bson:"domain"`
-
+	Id       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Email    string             `json:"email" bson:"email"`
+	Fullname string             `json:"fullname" bson:"fullname"`
+	Domain   string             `json:"domain" bson:"domain"`
 }
