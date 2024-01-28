03e1a6ec84061abb369565ee96660d4caa320cea
ahmadexe <muahmad710@gmail.com>
2024-01-29 03:35:08 +0500

    chore: format changes

--- services/auth/models/auth_data.go
+++ services/auth/models/auth_data.go
@@ -3,9 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AuthData struct {
-	Id       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Email    string             `json:"email" bson:"email"`
-	Fullname string             `json:"fullname" bson:"fullname"`
-	Domain   string             `json:"domain" bson:"domain"`
-	CreatedAt int64             `json:"createdAt" bson:"createdAt"`
+	Id        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Email     string             `json:"email" bson:"email"`
+	Fullname  string             `json:"fullname" bson:"fullname"`
+	Domain    string             `json:"domain" bson:"domain"`
+	CreatedAt int64              `json:"createdAt" bson:"createdAt"`
 }
