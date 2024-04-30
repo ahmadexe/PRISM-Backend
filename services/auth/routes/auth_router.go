beee42878f6a4b3642968039b7430e9444c6846f
ahmadexe <muahmad710@gmail.com>
2024-04-30 12:38:52 +0500

    chore(refac): proper encapsulation for data layer added

--- services/auth/routes/auth_router.go
+++ services/auth/routes/auth_router.go
@@ -7,21 +7,21 @@ import (
 )
 
 type AuthRouter struct {
-	AuthHandler *handlers.AuthHandler
-	Router *gin.Engine
+	authHandler *handlers.AuthHandler
+	router *gin.Engine
 }
 
 func InitAuthRouter(authHandler *handlers.AuthHandler, router *gin.Engine) *AuthRouter {
-	return &AuthRouter{AuthHandler: authHandler, Router: router}
+	return &AuthRouter{authHandler: authHandler, router: router}
 }
 
 func (router *AuthRouter) SetupRoutes() {
-	authMiddleware := middlewares.InitAuthMiddleware(router.AuthHandler.App)
-	auth := router.Router.Group("/auth")
+	authMiddleware := middlewares.InitAuthMiddleware(router.authHandler.App)
+	auth := router.router.Group("/auth")
 	auth.Use(authMiddleware.VerifyUser)
 	{
-		auth.POST("/users", router.AuthHandler.AddUser)
-		auth.GET("/users/:id", router.AuthHandler.GetUserById)
-		auth.PUT("/users", router.AuthHandler.UpdateUser)
+		auth.POST("/users", router.authHandler.AddUser)
+		auth.GET("/users/:id", router.authHandler.GetUserById)
+		auth.PUT("/users", router.authHandler.UpdateUser)
 	}
 }
