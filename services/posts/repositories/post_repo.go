84b18c0465a9cf3b66210ad02ac531e2716ae7af
ahmadexe <muahmad710@gmail.com>
2024-05-01 15:33:37 +0500

    fix: proper context added for timeout

--- services/posts/repositories/post_repo.go
+++ services/posts/repositories/post_repo.go
@@ -1,5 +1,32 @@
 package repositories
 
-type PostRepo struct {
+import (
+	"log"
+	"net/http"
 
+	"github.com/ahmadexe/prism-backend/services/posts/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type PostRepo struct {
+	collection *mongo.Collection
+}
+
+func InitPostRepo(client *mongo.Client) *PostRepo {
+	collection := client.Database("prism-dev").Collection("posts")
+	return &PostRepo{collection: collection}
+}
+
+// AddPost adds a post to the database
+func (repo *PostRepo) AddPost(post models.Post, ctx *gin.Context) {
+	_, err := repo.collection.InsertOne(ctx, post)
+
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error adding post to database. Please try again later."})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, post)
 }
