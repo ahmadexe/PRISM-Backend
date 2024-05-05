50dfc7a527b5decd35367496d0544dc5e282f392
ahmadexe <muahmad710@gmail.com>
2024-05-05 22:58:08 +0500

    chore: schema updates

--- services/posts/models/post.go
+++ services/posts/models/post.go
@@ -7,19 +7,20 @@ import (
 )
 
 type Post struct {
-	Id           primitive.ObjectID `json:"id" bson:"_id"`
-	UserId       string             `json:"userId" bson:"userId" validate:"required"`
-	ImageUrl     *string            `json:"imageUrl" bson:"imageUrl" validate:"required_without=Caption"`
-	Caption      *string            `json:"caption" bson:"caption" validate:"required_without=ImageUrl"`
-	NoOfViews    int                `json:"noOfViews" bson:"noOfViews"`
-	UserName     string             `json:"userName" bson:"userName"`
-	Category     string             `json:"category" bson:"category"`
-	UpVotes      int                `json:"upVotes" bson:"upVotes"`
-	DownVotes    int                `json:"downVotes" bson:"downVotes"`
-	NoOfComments int                `json:"noOfComments" bson:"noOfComments"`
-	IsBanned     bool               `json:"isBanned" bson:"isBanned"`
-	TotalReports int                `json:"totalReports" bson:"totalReports"`
-	CreatedAt    int64              `json:"createdAt" bson:"createdAt"`
+	Id             primitive.ObjectID `json:"id" bson:"_id"`
+	UserId         string             `json:"userId" bson:"userId" validate:"required"`
+	ImageUrl       *string            `json:"imageUrl" bson:"imageUrl" validate:"required_without=Caption"`
+	Caption        *string            `json:"caption" bson:"caption" validate:"required_without=ImageUrl"`
+	NoOfViews      int                `json:"noOfViews" bson:"noOfViews"`
+	UserName       string             `json:"userName" bson:"userName" validate:"required"`
+	Category       string             `json:"category" bson:"category" validate:"required"`
+	UserProfilePic string             `json:"userProfilePic" bson:"userProfilePic" validate:"required"`
+	UpVotes        int                `json:"upVotes" bson:"upVotes"`
+	DownVotes      int                `json:"downVotes" bson:"downVotes"`
+	NoOfComments   int                `json:"noOfComments" bson:"noOfComments"`
+	IsBanned       bool               `json:"isBanned" bson:"isBanned"`
+	TotalReports   int                `json:"totalReports" bson:"totalReports"`
+	CreatedAt      int64              `json:"createdAt" bson:"createdAt"`
 }
 
 func (post *Post) MarshalJSON() ([]byte, error) {
