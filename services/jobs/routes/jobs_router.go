3b06a2bffb298f2938c13312eae802214fc4669d
ahmadexe <muahmad710@gmail.com>
2024-09-09 00:57:03 +0500

    feat: router added for jobs

--- services/jobs/routes/jobs_router.go
+++ services/jobs/routes/jobs_router.go
@@ -20,5 +20,13 @@ func (router *JobsRouter) SetupRoutes() {
 		jobs.GET("/", func(ctx *gin.Context) {
 			ctx.JSON(200, gin.H{"message": "Welcome to Prism Jobs Service"})
 		})
+		jobs.POST("/jobs", router.jobsHandler.CreateJob)
+		jobs.GET("/jobs", router.jobsHandler.GetJobs)
+		jobs.GET("/jobs/:id", router.jobsHandler.GetJob)
+		jobs.PUT("/jobs/apply", router.jobsHandler.ApplyJob)
+		jobs.PUT("/jobs", router.jobsHandler.UpdateJob)
+		jobs.DELETE("/jobs/:id", router.jobsHandler.DeleteJob)
+		jobs.PUT("/jobs/like", router.jobsHandler.LikeJob)
+		jobs.PUT("/jobs/hire", router.jobsHandler.HireApplicant)
 	}
 }
