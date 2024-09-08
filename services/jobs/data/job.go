853f9b7949984afa8f79b5e1e0a1ed91fa068f7f
ahmadexe <muahmad710@gmail.com>
2024-09-08 05:00:02 +0500

    feat: job services setup && job model added

---
+++ services/jobs/data/job.go
@@ -0,0 +1,29 @@
+package data
+
+import (
+	"github.com/go-playground/validator"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Job struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	PostedBy    primitive.ObjectID `json:"postedBy" bson:"postedBy" validate:"required"`
+	Title       string             `json:"title" bson:"title" validate:"required"`
+	Description string             `json:"description" bson:"description" validate:"required"`
+	PostedAt    int32              `json:"postedAt" bson:"postedAt" validate:"required"`
+	Country     string             `json:"country" bson:"country" validate:"required"`
+	Keywords    []string           `json:"keywords" bson:"keywords" validate:"required"`
+	LikedBy     []string           `json:"likedBy" bson:"likedBy"`
+	AppliedBy   []string           `json:"appliedBy" bson:"appliedBy"`
+	Hired       primitive.ObjectID `json:"hired" bson:"hired"`
+	HiredAt     int32              `json:"hiredAt" bson:"hiredAt"`
+	Budget      float32            `json:"budget" bson:"budget" validate:"required"`
+	BudgetMeta  string             `json:"budgetMeta" bson:"budgetMeta" validate:"required"`
+	Username    string             `json:"username" bson:"username" validate:"required"`
+	Avatar      string             `json:"avatar" bson:"avatar"`
+}
+
+func (job *Job) Validate() error {
+	v := validator.New()
+	return v.Struct(job)
+}
